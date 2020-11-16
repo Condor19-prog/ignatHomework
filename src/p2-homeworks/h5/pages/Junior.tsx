da0fcac224c00c377c7228e95c917c2ec9ddaa8d
yauheni <yauheniauchynnikau19@gmail.com>
2020-11-17 00:15:25 +0300

    add task 6

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
+import HW6 from "../../h6/HW6";
 
 
 function Junior() {
 
-    return <NavLink to={'/JUNIOR'}>
-
-    </NavLink>
+    return (
+        <div>
+            <h3>Junior</h3>
+            <div>
+                Task 6 <br/>
+                <HW6/>
+            </div>
+        </div>
+    )
 }
 
 export default Junior
